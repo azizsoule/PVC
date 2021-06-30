eb7bd0b32641dd46530167afd71477ae7f2ea522
romaricano <jeannoelromaricano@gmail.com>
2021-06-30 02:18:29 +0000

    login test

--- src/main/java/com/projet/pvc/controllers/LoginCaissierController.java
+++ src/main/java/com/projet/pvc/controllers/LoginCaissierController.java
@@ -1,15 +1,36 @@
 package com.projet.pvc.controllers;
 
+import com.projet.pvc.repository.CaissierRepository;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class LoginCaissierController {
 
+    @Autowired
+    private CaissierRepository caissierRepository;
+
     @FXML
     AnchorPane window;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField password;
+
+    public void connect(ActionEvent actionEvent) {
+        if(caissierRepository.existsByLoginAndPassword(login.getText(),password.getText())) System.out.println("connecté");
+        else System.out.println("non connecté");
+    }
 
     @FXML
     void onExit() {
