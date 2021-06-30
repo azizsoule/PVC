13e1d1b05983c3780bfb2c1fc39a90d33d256b47
azizsoule <Abdul Aziz1>
2021-06-30 15:05:50 +0000

    adding vente

--- src/main/java/com/projet/pvc/utils/Provider.java
+++ src/main/java/com/projet/pvc/utils/Provider.java
@@ -2,12 +2,23 @@ package com.projet.pvc.utils;
 
 import com.projet.pvc.entities.Caissier;
 import com.projet.pvc.entities.Pvc;
+import com.projet.pvc.entities.Vente;
 
 public class Provider {
 
     private static Caissier caissier;
 
     private static Pvc pvc;
+
+    private static Vente vente;
+
+    public static Vente getVente() {
+        return vente;
+    }
+
+    public static void setVente(Vente vente) {
+        Provider.vente = vente;
+    }
 
     public static Pvc getPvc() {
         return pvc;
